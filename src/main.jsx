1172fd0e21547150eee73bcede8d564bf75b8468
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-03-08 15:47:14 -0300

    Feat: Criando estilização global

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Details } from "./pages/Details/index.jsx";
+import GlobalStyles from "./styles/global.js";
 import { ThemeProvider } from "styled-components";
 
 import theme from "./styles/theme.js";
@@ -8,6 +9,7 @@ import theme from "./styles/theme.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Details />
     </ThemeProvider>
   </React.StrictMode>
