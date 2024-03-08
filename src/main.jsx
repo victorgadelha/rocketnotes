1204104714ebea81fa84fd8cd63f2882c9cc2256
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-03-08 15:37:03 -0300

    feat: Criando tema global

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Details } from "./pages/Details/index.jsx";
+import { ThemeProvider } from "styled-components";
+
+import theme from "./styles/theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Details />
+    <ThemeProvider theme={theme}>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,10 @@
-import "./styles.css";
+import { Container } from "./styles";
 
 export function Details() {
   return (
-    <>
+    <Container>
       <h1>Hello World</h1>
       <span>João Victor Gadelha</span>
-    </>
+    </Container>
   );
 }
