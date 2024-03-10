c4ae6c4b808dabe39d93ae97ebb9b4521e79a522
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-03-09 21:53:36 -0300

    Feat: Criando página do usuário

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import { ThemeProvider } from "styled-components";
 //import { Details } from "./pages/Details;
 //import { Home } from "./pages/Home";
 //import { SignIn } from "./pages/SignIn";
-import { SignUp } from "./pages/SignUp";
+//import { SignUp } from "./pages/SignUp";
+import { Profile } from "./pages/Profile";
 
 import theme from "./styles/theme.js";
 
@@ -14,7 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,27 @@
+import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { Container, Form } from "./styles";
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft />
+        </a>
+      </header>
+
+      <Form>
+        <Input placeholder="Nome" type="text" icon={FiUser} />
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input placeholder="Senha atual" type="text" icon={FiLock} />
+        <Input placeholder="Nova atual" type="text" icon={FiLock} />
+
+        <Button title="Salvar"></Button>
+      </Form>
+    </Container>
+  );
+}
