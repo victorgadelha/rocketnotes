4296cd0c16e8ea15c5d7b34847cbe4fa7b241655
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-03-12 20:41:43 -0300

    feat: Finalizando a página New

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -3,6 +3,7 @@ import { Input } from "../../components/Input";
 import { Textarea } from "../../components/Textarea";
 import { NoteItem } from "../../components/NoteItem";
 import { Section } from "../../components/Section";
+import { Button } from "../../components/Button";
 
 import { Container, Form } from "./styles";
 
@@ -24,6 +25,13 @@ export function New() {
             <NoteItem value="https://rocketseat.com.br" />
             <NoteItem isNew placeholder="Novo link" />
           </Section>
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="react"></NoteItem>
+              <NoteItem isNew placeholder="Nova tag"></NoteItem>
+            </div>
+          </Section>
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
